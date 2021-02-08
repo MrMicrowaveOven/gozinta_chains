4ae21c27ec8d76d93e9dc2f957de34b1d191b676
Benjamin Zagorski <bzagorski@rhumbix.com>
2021-02-08 14:35:12 -0800

    Finished how_many_primes

--- primes.py
+++ primes.py
@@ -1,17 +1,25 @@
 import math
 
-def how_many_primes():
-    return 1
+def how_many_primes(n):
+    total = 0
+    for i in range(2, n):
+        if i % (10**9) == 0:
+            print(i)
+        if is_prime(i):
+            total += 1
+    return total
 
 def is_prime(n):
-    for i in range(math.ceil(math.sqrt(n))):
-        if i == 0 or i == 1:
-            continue
+    if n == 2:
+        return True
+    for i in range(2, math.ceil(math.sqrt(n) + 1)):
         if n % i == 0:
             return False
     return True
 
-print(is_prime(2))
-print(is_prime(5))
-print(is_prime(8))
-print(is_prime(12))
+# print(is_prime(2))
+# print(is_prime(5))
+# print(is_prime(8))
+# print(is_prime(12))
+
+print("Total: " + str(how_many_primes(10**12)))
